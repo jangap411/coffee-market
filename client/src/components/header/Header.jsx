20d957179e151ad0f1705a4c4259893c0df64a1c
Jedidiah Angap <77943305+jangap411@users.noreply.github.com>
2023-05-17 13:27:31 +1000

    Update Header.jsx

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./header.css";
-import { logo } from "../../assets";
+// import { logo } from "../../assets";
 // import { IonSearchbar } from "@ionic/react";
 // import {search-outline} from 'ionicons'
 
