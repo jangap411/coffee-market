948ef4d95a7ffec8490d581ceb9b15e8c59e7cbe
Jedidiah Angap <77943305+jangap411@users.noreply.github.com>
2023-05-18 14:45:09 +1000

    Update Footer.jsx

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { footer_bottom_img } from "../../assets";
+import { logo,footer_bottom_img } from "../../assets";
 import MenuItems from "../header/MenuItems";
 
 const Footer = () => {
