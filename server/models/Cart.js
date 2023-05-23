e9951c1053a8f9a1ee2b42fc34501d4c9d0518cb
JANGAP411 <angapj@gmail.com>
2023-05-23 16:24:19 +1000

    Added cart & order model, with basic fields

--- server/models/Cart.js
+++ server/models/Cart.js
@@ -1,6 +1,13 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
-const Cart = sequelize.define("", {});
+const Cart = sequelize.define("", {
+  user_id: {
+    type: DataTypes.INTEGER,
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+  },
+});
 
 module.exports = Cart;
