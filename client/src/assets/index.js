89f15a2c7020a4d01bd35535b4f4ceace36cf0b5
Alefay Kombil <alefay.bunes@gmail.com>
2023-05-26 06:53:03 +1000

    CSS Updates

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,21 +1,3 @@
-import {
-  logo,
-  upcoming_1,
-  upcoming_2,
-  upcoming_3,
-  upcoming_4,
-  hero_bg,
-  dry_beans,
-  footer_bottom_img,
-} from "./images";
+import { logo, hero_bg, dry_beans, footer_bottom_img } from "./images";
 
-export {
-  logo,
-  upcoming_1,
-  upcoming_2,
-  upcoming_3,
-  upcoming_4,
-  dry_beans,
-  hero_bg,
-  footer_bottom_img,
-};
+export { logo, dry_beans, hero_bg, footer_bottom_img };
