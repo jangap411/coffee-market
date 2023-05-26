7dc3db4fa129ab70bb3a86234606562f7eace331
jangap411 <angapj@gmail.com>
2023-05-26 21:48:46 +1000

    Update models to match db table collumns

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
 const User = sequelize.define(
-  "Users",
+  "users",
   {
     id: {
       type: DataTypes.INTEGER,
