72e9e2bef9cfdb2a53136a4d87bbf9d6e94e5ceb
Alefay Kombil <alefay.bunes@gmail.com>
2023-05-30 14:33:26 +1000

    URL Update

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,41 +1,29 @@
 import React from "react";
-// import { logo } from "../assets/index";
+import { logo } from "../assets/index";
 const Nav = () => {
-  return (
-    <nav id="nav">
-      <div className="navTop">
-        <div className="navItem">
-          <img src={logo} alt="kofi maketples" width="300" height="50" />
-        </div>
-        <div className="navItem">
-          <div className="search">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="searchInput"
-            />
-            <img
-              src="./img/search.png"
-              width="20"
-              height="20"
-              alt=""
-              className="searchIcon"
-            />
-          </div>
-        </div>
-        {/* <div className="navItem">
-          <span className="limitedOffer">Limited Offer!</span>
-        </div> */}
-      </div>
-      <div className="navBottom">
-        <h3 className="menuItem">BUYER</h3>
-        <h3 className="menuItem">FARMER</h3>
-        <h3 className="menuItem">EXPORTER</h3>
-        {/* <h3 className="menuItem">CRATER</h3>
-        <h3 className="menuItem">HIPPIE</h3> */}
-      </div>
-    </nav>
-  );
+	return (
+		<nav id="nav">
+			<div className="navTop">
+				<div className="navItem">
+					<img src={logo} alt="kofi maketples" width="300" height="50" />
+				</div>
+				<div className="navItem">
+					<div className="search">
+						<input
+							type="text"
+							placeholder="Search..."
+							className="searchInput"
+						/>
+					</div>
+				</div>
+			</div>
+			<div className="navBottom">
+				<h3 className="menuItem">BUYER</h3>
+				<h3 className="menuItem">FARMER</h3>
+				<h3 className="menuItem">EXPORTER</h3>
+			</div>
+		</nav>
+	);
 };
 
 export default Nav;
