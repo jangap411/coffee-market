1db7c6b2e59aef8d62d132b800c85fc5435cf9a5
JANGAP411 <angapj@gmail.com>
2023-07-07 16:22:33 +1000

    Added admin dashboard

--- client/src/pages/register/SignUp.jsx
+++ client/src/pages/register/SignUp.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
+import {
+  Button,
+  CssBaseline,
+  TextField,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+} from "@mui/material";
+
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { logo } from "../../assets";
 import Copyright from "../../components/Copyright";
